c77deb3ef5d8f16f86de7c3977f18c076a296bea
Yiming Lin <yl6@pdx.edu>
2019-12-03 11:23:37 -0800

    eval connected black and white

--- minimax_utility.py
+++ minimax_utility.py
@@ -1,9 +1,5 @@
-#import copy
-#import random
-
 from board import (Board, Move, ILLEGAL_MOVE, CONTINUE, 
   GAME_OVER, PLAYER_BLACK, PLAYER_WHITE)
-
 
 class MinimaxUtility(Board):
 
@@ -14,13 +10,15 @@ class MinimaxUtility(Board):
     self.evaluate_method = eval_method
 
     # a set of connected stones
-    self.connected_stones = set()
+    self.connected_black = set()
+    self.connected_white = set()
 
   def evaluate(self):
     if self.evaluate_method == "number":
       return self.__evaluate_number()
     elif self.evaluate_method == "connected":
-      return self.__evaluate_number() * 4 + self.__evaluate_connected()
+      nc_black, nc_white = self.__evaluate_connected()
+      return self.__evaluate_number() * 4 + 3 * nc_black - 1 * nc_white 
     else:
       raise Exception("unexpected evaluate method in minimax")
 
@@ -34,25 +32,31 @@ class MinimaxUtility(Board):
           score -= 1
     return score
 
-  def __evaluate_connected(self):
-    return len(self.connected_stones)
+  def __evaluate_connected(self): 
+    # XXX should evalueate maximum connected stones rather than total number XXX
+    return len(self.connected_black), len(self.connected_white)
 
   def update_connected_stones(self):  
     # update connected stones every time after try_move(), 
     # and before making decision
-    self.connected_stones = set()
+    self.connected_black = self.__update_connected_stones(PLAYER_BLACK)
+    self.connected_white = self.__update_connected_stones(PLAYER_WHITE)
+
+  def __update_connected_stones(self, side):  
+    connected = set()
     for i in range(5):
       for j in range(5):
         if (j < 4 
-            and self.board[i][j] == PLAYER_BLACK 
-            and self.board[i][j + 1] == PLAYER_BLACK):
-          self.connected_stones.add((i, j))
-          self.connected_stones.add((i, j + 1))
+            and self.board[i][j] == side 
+            and self.board[i][j + 1] == side):
+          connected.add((i, j))
+          connected.add((i, j + 1))
         if (i < 4 
-            and self.board[i][j] == PLAYER_BLACK 
-            and self.board[i + 1][j] == PLAYER_BLACK):
-          self.connected_stones.add((i, j))
-          self.connected_stones.add((i + 1, j))
+            and self.board[i][j] == side 
+            and self.board[i + 1][j] == side):
+          connected.add((i, j))
+          connected.add((i + 1, j))
+    return connected
      
   def move_ordering(self, moves):
     if not moves: # no possible moves
@@ -116,7 +120,7 @@ class MinimaxUtility(Board):
     if move.is_pass:
       return False 
     x, y = move.x, move.y
-    for stone in self.connected_stones:
+    for stone in self.connected_black:
       if ((x - 1 == stone[0] and y == stone[1]) or
           (x + 1 == stone[0] and y == stone[1]) or
           (x == stone[0] and y - 1 == stone[1]) or
